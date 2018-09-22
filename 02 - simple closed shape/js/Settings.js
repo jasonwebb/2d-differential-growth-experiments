722cd2e3da9f61f34cb798892fa1b80eaa2b990b
Jason Webb <zen.webb@gmail.com>
2018-09-22 18:15:43 -0500

    Tweaking settings for better forms

--- 02 - simple closed shape/js/Settings.js
+++ 02 - simple closed shape/js/Settings.js
@@ -1,15 +1,16 @@
 module.exports = {
-  MinDistance: 20, // injection, attraction
-  MaxDistance: 30, // split
-  RepulsionRadius: 20,
+  MinDistance: 1,
+  MaxDistance: 5,
+  RepulsionRadius: 10,
   MaxVelocity: .1,
   Acceleration: .1,
-  AttractionForce: .001,
-  RepulsionForce: 500,
-  AlignmentForce: .001,
+  AttractionForce: .2,
+  RepulsionForce: .5,
+  AlignmentForce: .5,
   ScreenPadding: 20,
   MaxNodes: 3000,
-  NodeInjectionInterval: 100,
+  NodeInjectionInterval: 1,
   DrawNodes: false,
-  TraceMode: false
+  TraceMode: false,
+  InvertedColors: false
 }
