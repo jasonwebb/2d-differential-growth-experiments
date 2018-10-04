6d91b6ca6d3ebcecbbbdf2fcab20a7f86d3e6893
Jason Webb <zen.webb@gmail.com>
2018-10-03 20:23:55 -0500

    Adding keyboard control of starting form, adding superformula SVG

--- 04 - SVG as input/js/entry.js
+++ 04 - SVG as input/js/entry.js
@@ -6,6 +6,10 @@ let Node = require('../../core/Node'),
 
 let world;
 
+const DIFFERENTIAL_GROWTH = 'differential-growth',
+      HELLO_WORLD_SERIF = 'hello-world-serif',
+      SUPERFORMULA = 'superformula';
+let currentFile = HELLO_WORLD_SERIF;
 
 /*
 =============================================================================
@@ -31,9 +35,9 @@ const sketch = function (p5) {
     world.draw();
 
     // Draw canvas bounds for alignment with video recording software
-    // p5.noFill();
-    // p5.stroke(200);
-    // p5.rect(window.innerWidth/2 - 1, window.innerHeight/2 - 1, 720 + 2, 900 + 4);
+    p5.noFill();
+    p5.stroke(0);
+    p5.rect(window.innerWidth/2 - 1, window.innerHeight/2 - 1, 900 - 100, 900 - 100);
   }
 
   // Restart the simulation ---------------------------------------------
@@ -41,48 +45,77 @@ const sketch = function (p5) {
     world.clearPaths();
 
     // Load the default SVG file
-    let svg = document.getElementById('differential-growth');
+    let svg = document.getElementById(currentFile);
     let inputPaths = svg.contentDocument.querySelectorAll('path');
     let currentPath = new Path(p5, [], Settings, true);
 
     // Scrape all points from all points, and record breakpoints
-    for(let i = 0; i < 4; i++) {
-      for(let inputPath of inputPaths) {
-        let pathData = new SVGPathData(inputPath.getAttribute('d'));
+    for(let [pathIndex, inputPath] of inputPaths.entries()) {
+      let pathData = new SVGPathData(inputPath.getAttribute('d'));
 
-        let previousCoords = {
-          x: 500,
-          y: 500
-        };
+      let previousCoords = {
+        x: 0,
+        y: 0
+      };
 
-        for(let command of pathData.commands) {
-          switch(command.type) {
-            case SVGPathData.MOVE_TO:
-            case SVGPathData.LINE_TO:
-              currentPath.addNode(new Node(p5, command.x, command.y, Settings));
-              break;
-            case SVGPathData.HORIZ_LINE_TO:
-            currentPath.addNode(new Node(p5, command.x, previousCoords.y, Settings));
-              break;
-            case SVGPathData.VERT_LINE_TO:
-              currentPath.addNode(new Node(p5, previousCoords.x, command.y, Settings));
-              break;
-            case SVGPathData.CLOSE_PATH:
-              currentPath.scale(2.25);
-              currentPath.moveTo(window.innerWidth/2 - 400, i*210 - 70);
-              world.addPath(currentPath);
-              currentPath = new Path(p5, [], Settings, true);
-              break;
-          }
+      for(let command of pathData.commands) {
+        switch(command.type) {
+          // Move ('M') and line ('L') commands have both X and Y
+          case SVGPathData.MOVE_TO:
+          case SVGPathData.LINE_TO:
+            currentPath.addNode(new Node(p5, command.x, command.y, Settings));
+            break;
 
-          if(command.hasOwnProperty('x')) {
-            previousCoords.x = command.x;
-          }
+          // Horizontal line ('H') commands only have X, using previous command's Y
+          case SVGPathData.HORIZ_LINE_TO:
+          currentPath.addNode(new Node(p5, command.x, previousCoords.y, Settings));
+            break;
 
-          if(command.hasOwnProperty('y')) {
-            previousCoords.y = command.y;
-          }
+          // Vertical line ('V') commands only have Y, using previous command's X
+          case SVGPathData.VERT_LINE_TO:
+            currentPath.addNode(new Node(p5, previousCoords.x, command.y, Settings));
+            break;
+
+          // ClosePath ('Z') commands are a naive indication that the current path can be processed and added to the world
+          case SVGPathData.CLOSE_PATH:
+            switch(currentFile) {
+              case DIFFERENTIAL_GROWTH:
+                currentPath.scale(2.5);
+                currentPath.moveTo(window.innerWidth/2 - 440, window.innerHeight/2 - 300);
+                break;
+
+              case HELLO_WORLD_SERIF:
+                currentPath.scale(2.25);
+                currentPath.moveTo(window.innerWidth/2 - 300, window.innerHeight/2 - 410);
+                break;
+            }
+
+            // Add path to the world
+            world.addPath(currentPath);
+
+            // Set up a new empty Path for the next loop iterations
+            currentPath = new Path(p5, [], Settings, true);
+            currentPath.setInvertedColors(true);
+            break;
         }
+
+        // Capture X coordinate, if there was one
+        if(command.hasOwnProperty('x')) {
+          previousCoords.x = command.x;
+        }
+
+        // Capture Y coordinate, if there was one
+        if(command.hasOwnProperty('y')) {
+          previousCoords.y = command.y;
+        }
+      }
+
+      // The superformula file does not have a ClosePath ('Z'), so we have to make do by watching for last iteration for now
+      if(currentFile == SUPERFORMULA && pathIndex == inputPaths.length - 1) {
+        currentPath.scale(.7);
+        currentPath.moveTo(280, 150);
+        world.addPath(currentPath);
+        currentPath = new Path(p5, [], Settings, true);
       }
     }
     
@@ -104,7 +137,25 @@ const sketch = function (p5) {
   =============================================================================
   */
   p5.keyReleased = function() {
-    switch (p5.key) {  
+    switch (p5.key) {
+      // Switch to first SVG with '1'
+      case '1':
+        currentFile = HELLO_WORLD_SERIF;
+        restartWorld();
+        break;
+
+      // Switch to second SVG with '2'
+      case '2':
+        currentFile = DIFFERENTIAL_GROWTH;
+        restartWorld();
+        break;
+
+      // Switch to third SVG with '3'
+      case '3':
+        currentFile = SUPERFORMULA;
+        restartWorld();
+        break;
+
       // Toggle trace mode with 't'
       case 't':
         world.toggleTraceMode()
