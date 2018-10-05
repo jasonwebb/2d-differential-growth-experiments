1843a6beb03e036bb8c9a3df4f6f46637c78f6d0
Jason Webb <zen.webb@gmail.com>
2018-10-04 20:10:15 -0500

    Adding functions to create arcs made of lines to experiment 04

--- 05 - line studies/js/entry.js
+++ 05 - line studies/js/entry.js
@@ -8,8 +8,9 @@ let world;
 const HORIZONTAL = 0,
       VERTICAL = 1,
       ANGLED = 2,
-      RADIAL = 3;
-let currentLineType = RADIAL;
+      RADIAL = 3,
+      OPPOSING_ARCS = 4;
+let currentLineType = OPPOSING_ARCS;
 
 
 /*
@@ -39,7 +40,7 @@ const sketch = function (p5) {
 
     // Draw canvas bounds for alignment with video recording software
     p5.noFill();
-    p5.stroke(0);
+    p5.stroke(255);
     p5.rect(window.innerWidth/2, window.innerHeight/2, 900 - 100, 900 - 100);
   }
 
@@ -99,14 +100,12 @@ const sketch = function (p5) {
     createLines(rows, columns, 38, 20, -30, 30);
   }
 
-  // Radial arrangement of lines
-  function createRadialLines() {
-    let linesPerRev = 60;
-    let angleDelta = 360 / linesPerRev;
-    let innerRadius = 75;
-    let outerRadius = 100;
+  // Create an arc composed of lines
+  function createArcLines(centerX, centerY, degreesStart, degreesEnd, innerRadius, linesPerArc, lineLength) {
+    let angleDelta = (degreesEnd - degreesStart) / linesPerArc;
+    let outerRadius = innerRadius + lineLength;
 
-    for(let i = 0; i < linesPerRev * angleDelta; i += angleDelta) {
+    for(let i = degreesStart; i < degreesEnd; i += angleDelta) {
       let nodes = [];
 
       // Innermost Node
@@ -126,13 +125,24 @@ const sketch = function (p5) {
       );
 
       let path = new Path(p5, nodes, Settings);
-      path.moveTo(window.innerWidth/2, window.innerHeight/2);
+      path.moveTo(centerX, centerY);
 
       world.addPath(path);
     }
   }
 
-  // Restart the simulation with a selected path type -------------------
+  // Create two arcs of lines in opposing corners of the canvas
+  function createOpposingArcs() {
+    createArcLines(window.innerWidth/2 - 900/2 + 100, window.innerHeight/2 + 900/2 - 75, 270, 360, 375, 66, 100);
+    createArcLines(window.innerWidth/2 + 900/2 - 100, window.innerHeight/2 - 900/2 + 75, 90, 180, 375, 60, 100);
+  }
+
+  // Create a full ring of lines
+  function createLineRing() {
+    createArcLines(window.innerWidth/2, window.innerHeight/2, 0, 360, 75, 60, 50);
+  }
+
+  // Restart the simulation with the selected path type -------------------
   function restartWorld() {
     world.clearPaths();
 
@@ -148,7 +158,10 @@ const sketch = function (p5) {
         createAngledLines(15, 20);
         break;
       case RADIAL:
-        createRadialLines();
+        createLineRing();
+        break;
+      case OPPOSING_ARCS:
+        createOpposingArcs();
         break;
     }
     
@@ -188,6 +201,9 @@ const sketch = function (p5) {
         currentLineType = ANGLED;
         restartWorld();
         break;
+
+      case '4':
+        currentLineType
 
       // Switch to radial arrangement of lines with '4'
       case '4':
