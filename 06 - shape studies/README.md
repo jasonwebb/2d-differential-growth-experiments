39eb9ee27a06d1b6b085b1eecd36e947cac0f280
Jason Webb <zen.webb@gmail.com>
2018-10-15 19:21:23 -0500

    Adding experiment 06 - shape studies

---
+++ 06 - shape studies/README.md
@@ -0,0 +1,3 @@
+# 05 - line studies
+
+This experiment applies the differential growth process to a various arrangements of shapes.
---
+++ 06 - shape studies/js/Settings.js
@@ -0,0 +1,14 @@
+module.exports = {
+  MinDistance: 1,
+  MaxDistance: 5,
+  RepulsionRadius: 10,
+  AttractionForce: .2,
+  RepulsionForce: .6,
+  AlignmentForce: .55,
+  NodeInjectionInterval: 100,
+  DrawNodes: false,
+  TraceMode: false,
+  InvertedColors: false,
+  DebugMode: false,
+  FillMode: false
+}
